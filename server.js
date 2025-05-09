265718e207be482d045bdbadc5fa81b0dcef58b8
JaydeepGaikwad0 <jaydeep.gaikwad.it19@ggits.net>
2025-05-09 11:00:06 +0530

    Your update message

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.snvcas.com', 'http://localhost:5173'],
+  credentials: true
+}));
 // Increase payload size limit for file uploads
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
